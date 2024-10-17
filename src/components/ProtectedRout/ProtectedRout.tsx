ed6972e8432619bd5418f128b406b57138fd3c62
AlexP <86realmadrid86@gmail.com>
2024-10-18 02:19:57 +0300

    add protected route (without login yeat, just alwas navigate to login page)

--- src/components/ProtectedRout/ProtectedRout.tsx
+++ src/components/ProtectedRout/ProtectedRout.tsx
@@ -1,9 +1,20 @@
-import { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-interface ProtectedRoutProps {
+interface ProtectedRouteProps {
   children: ReactNode;
 }
 
-export const ProtectedRout: FC<ProtectedRoutProps> = ({ children }) => {
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return <>{children}</>;
 };
