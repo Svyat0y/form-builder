a5e2fb2e521f4bdb054b8e69735bde001793b4f6
Alexander Pop <50931164+Svyat0y@users.noreply.github.com>
2026-01-20 16:15:00 +0100

    Update 3.0.1 - error 401 is not catching by interceptors on the login and register pages
    * feat: error 401 is not catching by interceptors on the login and register pages

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -44,6 +44,14 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config
 
+    if (originalRequest.url.includes('/auth/login')) {
+      return Promise.reject(error)
+    }
+
+    if (originalRequest.url.includes('/auth/register')) {
+      return Promise.reject(error)
+    }
+
     if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
